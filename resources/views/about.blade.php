0239329e1a8e5ff718988432daebaeea59e0c3e5
Bayo <jbayo@iesebre.com>
2022-11-07 17:46:55 +0100

    Final video 110

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+    <div class="bg-gray-800 pb-32">
+        <header class="py-10">
+            <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold tracking-tight text-white">About</h1>
+            </div>
+        </header>
+        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+
+            <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias dolor dolorem ex
+                explicabo harum maiores maxime modi, molestias non numquam officia praesentium quae quam recusandae
+                reiciendis saepe sed sunt totam?</p>
+
+        </div>
+
+    </div>
+</x-layout>
